6b410ee04e0cbe6dab90f6e9754d8d1513b074de
Austin <austinpulley19@yahoo.com>
2022-02-04 22:06:28 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(number, number2) {
+    return (number + number2)
+}
+
+function subtract(sdf, hgg) {
+    return (sdf - hgg)
+}
+
+function multiply(digit1, digit2) {
+    return (digit1 * digit2)
+}
+
+function divide(num, num1) {
+    return (num / num1)
+}
+function increment(n) {
+    return (++n)
+}
+function decrement(m){
+    return (--m)
+}
+function makeInt(n){
+    return parseInt(n, 10);
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+}
